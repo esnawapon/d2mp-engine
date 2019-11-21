a64c202f9b91ef43abc40396e3d6e1aaaa29ce24
Es Nawapon Rattanakansaeng <es.nawapon@gmail.com>
2019-11-21 16:44:26 +0700

    implement predict and feedback api

--- src/main/java/com/d2mp/engine/controller/ApiController.java
+++ src/main/java/com/d2mp/engine/controller/ApiController.java
@@ -4,6 +4,9 @@ import com.d2mp.engine.model.PredictRequest;
 import java.util.HashMap;
 import java.util.Map;
 import javax.validation.Valid;
+
+import com.d2mp.engine.service.PredictionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +15,9 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("api")
 public class ApiController {
+    @Autowired
+    private PredictionService predictionService;
+
     public ResponseEntity success(Object object) {
         return new ResponseEntity<>(object == null ? "null" : object, HttpStatus.OK);
     }
@@ -25,10 +31,15 @@ public class ApiController {
 
     @PostMapping(path="/predict")
     public ResponseEntity predict(@Valid @RequestBody PredictRequest request) throws Exception {
-        Thread.sleep(1500);
+        boolean result = predictionService.predictWinResult(request);
         Map r = new HashMap();
-        r.put("win", Math.random() > 0.5);
-        r.put("correctness", Math.random());
+        r.put("win", result);
         return success(r);
+    }
+
+    @PostMapping(path="/feedback")
+    public ResponseEntity feedback(@Valid @RequestBody PredictRequest request) throws Exception {
+        predictionService.recordFeedback(request);
+        return success(null);
     }
 }
